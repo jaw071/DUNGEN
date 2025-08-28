ce42ea61756b0bed372a005142e303d0e4940be3
jaw071 <jaw071@student.usc.edu.au>
2025-08-28 14:18:38 +1000

    removed old cooking script and scene

--- Assets/DUNGEN ASSETS/Scripts/CookingUI/Slot.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-using MoreMountains.InventoryEngine;
-
-public class Slot : MonoBehaviour
-{
-    public InventoryItem item; // Holds the ingredient
-    public int index;          // Used by CookingManager to identify slot position
-}
